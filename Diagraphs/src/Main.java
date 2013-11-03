17059aa4d030e047ecdc8d302ff922359e6a1c97
ivanoreh456 <ivan.oreh@hotmail.com>
2013-11-03 11:05:35 +0100

    Main_update

--- Diagraphs/src/Main.java
+++ Diagraphs/src/Main.java
@@ -2,8 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
+		/*input int V = number of Vertex, E number of Edges (directed)
+		 * E times 2 ints ( V and W ) describing edge going from V to W
+		 */
+		int V= Integer.parseInt(args[0]);
+		int E =Integer.parseInt( args[1]);
+		Diagraph DG = new Diagraph(V);
 		
-
+		for( int i= 0; i<E*2; i++){
+			int A = Integer.parseInt(args[i+2]);
+			int B = Integer.parseInt(args[i+3]);
+			DG.addEdge(A, B);
+		}
 	}
 
 }
