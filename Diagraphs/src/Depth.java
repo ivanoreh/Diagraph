4b8679ff07f8a014dc0a8c250068da8cb1dceead
ivanoreh456 <ivan.oreh@hotmail.com>
2013-11-03 11:28:48 +0100

    Prije

--- Diagraphs/src/Depth.java
+++ Diagraphs/src/Depth.java
@@ -1,14 +1,17 @@
 
 public class Depth {
 	private boolean[] visited;
+	int[] prije;
 	
 	public Depth( Diagraph G, int v) {
 		visited = new boolean[v];
+		prije = new int[v];
 	}
 	public void dfs ( Diagraph G, int v ){
 		for( int i: G.adj.elementAt(v)){
 			if( !visited[i]){
 				visited[i]  = true;
+				prije[i] = v;
 				dfs( G, i);
 			}
 		}
