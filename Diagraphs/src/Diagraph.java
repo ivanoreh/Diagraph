9112a0e02bfdc2a39fb1f29ebc02c95874c65b45
Ivan <ivan.oreh@hotmail.com>
2015-05-07 11:04:36 +0200

    Update Diagraph.java

--- Diagraphs/src/Diagraph.java
+++ Diagraphs/src/Diagraph.java
@@ -4,19 +4,28 @@ public class Diagraph {
 	Vector< Vector<Integer>> adj = new Vector< Vector<Integer>> ();
 	int Vrhova;
 	
-	public Diagraph(int V) {
+	public Diagraph(int V) 
+	{
 		Vrhova = V;
 		Vector<Integer> novi= new Vector<Integer>();
 		for( int i=0; i<V; i++) adj.add(novi);
 	}
 	
 	
-	public void addEdge( int v, int w){
+	public void addEdge( int v, int w)
+	{
 		adj.elementAt(v).add(w);
 	}
 	
-	public Vector<Integer> veze (int v){
+	public Vector<Integer> veze (int v)
+	{
 		return adj.elementAt(v);
-		}
+	}
+	
+	public boolean isConnected (int x, int y)
+	{
+		BFS bfs = new BFS( this, x );
+		return bfs.visited[y];
+	}
 
 }
